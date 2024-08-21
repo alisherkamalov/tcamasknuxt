0830ad26fbb9766b81e632d1cb269710c646a70f
Bahtier Kamalov <bahtierkamalov21@gmail.com>
2024-08-21 18:08:36 +0500

    .

---
+++ components/TheHeader.vue
@@ -0,0 +1,390 @@
+<template>
+  <TheNav />
+  <div class="container">
+    <div class="left-header">
+      <span class="title">Made in Britain</span>
+      A game changer in the ethical fight against Covid-19
+      <span class="info"
+        >Textiles are one of the main vectors for viruses and bacteria. Textiles
+        treat with HeiQ viroblock help stop cross- contamination from surface to
+        face covering.</span
+      >
+      <div class="buttons">
+        <button class="shop-now">Shop Now</button>
+        <button class="learn-more">Learn More</button>
+      </div>
+    </div>
+    <div class="right-header">
+      <img src="/photo-header.png" alt="photo header" class="photo-header" />
+      <div class="cubes">
+        <div class="cube-one"></div>
+        <div class="cube-two"></div>
+        <div class="cube-three"></div>
+      </div>
+    </div>
+    <div class="bottom-header">
+      <svg
+        class="f-ico"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"
+      >
+        <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+        <path
+          fill="#6f7074"
+          d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"
+        />
+      </svg>
+      <svg
+        class="y-ico"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+      >
+        <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+        <path
+          fill="#6f7074"
+          d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"
+        />
+      </svg>
+      <svg
+        class="l-ico"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+        <path
+          fill="#6f7074"
+          d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+        />
+      </svg>
+      <svg
+        class="i-ico"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+        <path
+          fill="#6f7074"
+          d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.l-ico {
+  width: 19.88px;
+  height: 20px;
+}
+.i-ico {
+  width: 20px;
+  height: 20px;
+}
+.f-ico {
+  width: 10.39px;
+  height: 20px;
+}
+.y-ico {
+  position: relative;
+  top: 2.5px;
+  width: 22.01px;
+  height: 15.47px;
+}
+.bottom-header {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 730px;
+  padding-left: 60px;
+  height: 47px;
+  z-index: 2;
+  display: flex;
+  gap: 15px;
+  background-color: white;
+}
+.buttons {
+  display: flex;
+  gap: 25px;
+}
+.shop-now {
+  width: 140px;
+  height: 48px;
+  border-radius: 4px;
+  color: #006ff9;
+  border: 1px solid #006ff9;
+  font-size: 16px;
+  background-color: white;
+  font-weight: 400;
+  line-height: 24px;
+  text-transform: uppercase;
+  text-align: center;
+  transition: all 0.2s ease;
+}
+.learn-more {
+  width: 140px;
+  height: 48px;
+  border-radius: 4px;
+  border: 1px solid #006ff9;
+  color: #006ff9;
+  background-color: white;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  text-transform: uppercase;
+  text-align: center;
+  transition: all 0.2s ease;
+}
+.learn-more:hover {
+  background-color: #006ff9;
+  color: white;
+}
+.shop-now:hover {
+  background-color: #006ff9;
+  color: white;
+}
+.title {
+  font-size: 14px;
+  font-weight: 600;
+  text-transform: uppercase;
+  line-height: 12px;
+  letter-spacing: 2px;
+  text-align: left;
+  color: #006ff9;
+}
+.info {
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 25px;
+  color: #6f7074;
+  text-align: left;
+}
+.container {
+  width: 100%;
+  max-width: 1270px;
+  height: 751px;
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+.left-header {
+  width: 440px;
+  height: 341px;
+  position: relative;
+  display: flex;
+  gap: 15px;
+  flex-direction: column;
+  left: 55px;
+  font-size: 44px;
+  color: #212529;
+  font-weight: 500;
+  line-height: 52px;
+  text-align: start;
+}
+.right-header {
+  width: 515px;
+  height: 751px;
+  position: absolute;
+  right: 0;
+  z-index: 0;
+  display: flex;
+  align-items: center;
+  background-color: #0a236d;
+}
+.photo-header {
+  width: 500px;
+  height: 560.5px;
+  z-index: 1;
+  position: relative;
+  right: 190px;
+}
+.cubes {
+  min-width: 100%;
+  height: 751px;
+  position: absolute;
+  left: 0;
+  overflow: hidden;
+  display: flex;
+}
+.cube-one {
+  width: 110.31px;
+  height: 110.31px;
+  position: absolute;
+  bottom: 100px;
+  left: 125px;
+  background-color: #ffffff;
+  opacity: 10%;
+  rotate: 23.74deg;
+}
+.cube-two {
+  width: 110.31px;
+  height: 110.31px;
+  position: absolute;
+  top: 227px;
+  left: 213px;
+  background-color: #ffffff;
+  opacity: 10%;
+}
+.cube-three {
+  width: 164px;
+  position: absolute;
+  height: 164px;
+  bottom: 35px;
+  right: -79px;
+  background-color: #ffffff;
+  opacity: 10%;
+  overflow: hidden;
+  rotate: -45deg;
+}
+@media(min-width:300px) {
+    .photo-header {
+        scale: 0.4;
+        right: 170px;
+        top: -70px;
+    }
+    .title {
+        font-size: 10px;
+    }
+    .info {
+        font-size: 10px;
+        width: 100%;
+        line-height: 20px;
+    }
+    .left-header {
+        font-size: 19px;
+        left: 20px;
+        line-height: 35px;
+        width: 35%;
+        position: absolute;
+        top: 100px;
+        z-index: 10;
+    }
+    .right-header {
+        width: 50%;
+        top: 0;
+        height: 500px;
+       
+    }
+    .shop-now {
+        font-size: 12px;
+    }
+    .learn-more {
+        font-size: 12px;
+    }
+    .container {
+        height: 570px;
+    }
+
+}
+@media(min-width:600px) {
+    .shop-now {
+        font-size: 12px;
+    }
+    .learn-more {
+        font-size: 12px;
+    }
+    .photo-header {
+        scale: 0.5;
+        right: 150px;
+        top: -170px;
+    }
+    .title {
+        font-size: 10px;
+    }
+    .info {
+        font-size: 13px;
+        width: 80%;
+        line-height: 25px;
+    }
+    .left-header {
+        font-size: 34px;
+        width: 35%;
+        position: relative;
+        top: -100px;
+        z-index: 10;
+        line-height: 52px;
+    }
+    .right-header {
+        width: 50%;
+        top: 0;
+        right: 0;
+        height: 751px;
+    }
+    .container {
+        height: 751px;
+    }
+
+}
+@media (min-width:900px) {
+    .shop-now {
+        font-size: 16px;
+    }
+    .learn-more {
+        font-size: 16px;
+    }
+    .photo-header {
+        scale: 0.9;
+        right: 100px;
+        top: 0;
+    }
+    .title {
+        font-size: 10px;
+    }
+    .info {
+        font-size: 13px;
+        width: 200px;
+        line-height: 25px;
+    }
+    .left-header {
+        font-size: 34px;
+        width: 30%;
+        top: 0;
+        line-height: 52px;
+    }
+    .right-header {
+        width: 50%;
+        top: 0;
+        height: 751px;
+    }
+    .container {
+        height: 751px;
+    }
+}
+@media(min-width: 1270px) {
+    .shop-now {
+        font-size: 16px;
+    }
+    .learn-more {
+        font-size: 16px;
+    }
+    .photo-header {
+        scale: 1;
+        right: 190px;
+        top: 0;
+    }
+    .title {
+        font-size: 14px;
+    }
+    .info {
+        font-size: 16px;
+        width: 340px;
+        line-height: 25px;
+    }
+    .left-header {
+        font-size: 44px;
+        width: 440px;
+        top: 0;
+        left: 50px;
+        line-height: 52px;
+    }
+    .right-header {
+        width: 515px;
+        top: 0;
+        height: 751px;
+    }
+    .container {
+        height: 751px;
+    }
+}
+</style>
---
+++ components/TheNav.vue
@@ -0,0 +1,377 @@
+<template>
+  <nav>
+    <div class="container">
+      <img src="/logo.svg" alt="logo" class="logo" />
+      <button class="burger-btn" @click="BurgerMenu">
+        <div class="lines">
+          <div class="line" v-for="(line, index) in lines" :key="index"></div>
+        </div>
+      </button>
+      <ul class="ul-nav">
+        <li class="li-home">Home</li>
+        <li class="li-about">About</li>
+        <li class="li-secience">The Secience</li>
+        <li class="li-bulk">Shop Bulk</li>
+        <li class="li-ordering">Ordering</li>
+        <li class="li-news">News</li>
+      </ul>
+      <div class="buttons">
+        <button class="cart">
+          <img src="/cart.svg" alt="cart" class="cart-ico" />
+        </button>
+        <button class="contact">Contact</button>
+      </div>
+    </div>
+  </nav>
+  <div class="burger-menu" :style="{ display: dispmenu }">
+    <div
+      class="black-bg"
+      :style="{ opacity: blackbg }"
+      @click="BurgerMenu"
+    ></div>
+    <div
+      class="content-menu"
+      :style="{ transform: `translate(${offsetmenu})` }"
+    >
+      <img src="/logo.svg" alt="logo" class="logo-mob" />
+      <ul class="ul-nav-mob">
+        <li class="li-home">Home</li>
+        <li class="li-about">About</li>
+        <li class="li-secience">The Secience</li>
+        <li class="li-bulk">Shop Bulk</li>
+        <li class="li-ordering">Ordering</li>
+        <li class="li-news">News</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+export default {
+  setup() {
+    const lines = [{}, {}, {}];
+    const isOpen = ref(false);
+    const blackbg = ref(0);
+    const dispmenu = ref("none");
+    const offsetmenu = ref("-500px, 0px");
+
+    const BurgerMenu = () => {
+     
+
+      isOpen.value = !isOpen.value;
+      if (isOpen.value) {
+        dispmenu.value = "flex";
+        
+        setTimeout(() => {
+          offsetmenu.value = "0px, 0px";
+          blackbg.value = 0.5;
+        }, 10);
+      } else {
+        offsetmenu.value = "-500px, 0px";
+        blackbg.value = 0;
+        setTimeout(() => {
+          dispmenu.value = "none";
+          
+        }, 500);
+      }
+    };
+
+    onMounted(() => {
+      
+    });
+
+    return {
+      lines,
+      
+      isOpen,
+      blackbg,
+      offsetmenu,
+      BurgerMenu,
+      dispmenu,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.burger-menu {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: none;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 99;
+  transition: all 0.5s ease;
+}
+.content-menu {
+  width: 200px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  z-index: 101;
+  background-color: white;
+  transition: all 0.5s ease;
+}
+.line {
+  width: 25px;
+  height: 1.5px;
+
+  background-color: black;
+  border-radius: 5px;
+  transition: all 0.5s ease;
+}
+.lines {
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+  margin-bottom: 25px;
+  transition: all 0.5s ease;
+}
+.burger-btn {
+  border: 0;
+  display: none;
+  padding-left: 12px;
+  padding-top: 11px;
+  border-radius: 5px;
+  border: 1px solid grey;
+  padding-right: 12px;
+  height: 42px;
+  background-color: white;
+  position: absolute;
+  left: 25px;
+  transition: all 0.5s ease;
+}
+.burger-btn:active {
+  scale: 0.9;
+}
+.burger-btn:hover {
+  background-color: #006ff9;
+}
+.lines:hover > .line {
+  background-color: white;
+}
+
+.container {
+  width: 100%;
+  max-width: 1240px;
+  display: flex;
+  z-index: 4;
+  overflow: hidden;
+  justify-content: space-evenly;
+}
+.li-home,
+.li-about,
+.li-secience,
+.li-bulk,
+.li-ordering,
+.li-news {
+  transition: all 0.5s ease;
+  cursor: pointer;
+}
+.li-home:hover {
+  color: #006ff9;
+  translate: 0px -5px;
+}
+.li-about:hover {
+  color: #006ff9;
+  translate: 0px -5px;
+}
+.li-secience:hover {
+  color: #006ff9;
+  translate: 0px -5px;
+}
+.li-bulk:hover {
+  color: #006ff9;
+  translate: 0px -5px;
+}
+.li-ordering:hover {
+  color: #006ff9;
+  translate: 0px -5px;
+}
+.li-news:hover {
+  color: #006ff9;
+  translate: 0px -5px;
+}
+.buttons {
+  display: flex;
+  gap: 25px;
+  position: relative;
+  top: 2px;
+}
+.contact {
+  width: 108px;
+  height: 38px;
+  background-color: white;
+  border-radius: 4px;
+  border: 0;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.5s ease;
+}
+.contact:hover {
+  background-color: #006ff9;
+  color: white;
+}
+.cart-ico {
+  width: 18px;
+  height: 15.65px;
+  position: relative;
+  left: -1px;
+  top: 1px;
+}
+.cart {
+  width: 38px;
+  height: 38px;
+  border: 1px solid #006ff9;
+  background-color: white;
+  border-radius: 100%;
+  cursor: pointer;
+  transition: all 0.5s ease;
+}
+.cart:active {
+  scale: 0.9;
+}
+.logo {
+  width: 81.82px;
+  height: 48px;
+  margin-right: 25px;
+}
+.ul-nav {
+  position: relative;
+  left: -70px;
+  height: 47px;
+  display: flex;
+  gap: 25px;
+  background-color: white;
+  padding-right: 33.5px;
+  align-items: center;
+  font-size: 15px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: left;
+}
+.logo-mob {
+  width: 81.82px;
+  height: 48px;
+  position: relative;
+  top: 20px;
+  left: 25px;
+  margin-right: 25px;
+}
+.ul-nav-mob {
+  position: relative;
+  left: -70px;
+  height: 47px;
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  background-color: white;
+  padding-right: 33.5px;
+  align-items: left;
+  font-size: 15px;
+  font-weight: 500;
+  left: 30px;
+  top: 50px;
+  line-height: 20px;
+  text-align: left;
+}
+@media (min-width: 300px) {
+    nav {
+        width: 99.5%;
+    }
+  .buttons {
+    position: relative;
+    z-index: 4;
+    margin-right: 25px;
+  }
+  .ul-nav {
+    display: none;
+  }
+  .logo {
+    display: none;
+  }
+  .burger-btn {
+    display: flex;
+    margin-left: 9px;
+  }
+  .container {
+    justify-content: flex-end;
+    align-items: center;
+    height: 75px;
+  }
+}
+@media (min-width: 600px) {
+    nav {
+        width: 99.5%;
+    }
+  .logo {
+    display: none;
+  }
+  .ul-nav {
+    display: none;
+  }
+  .burger-btn {
+    display: flex;
+  }
+  .container {
+    justify-content: flex-end;
+    height: 75px;
+  }
+}
+@media (min-width: 900px) {
+    nav {
+        width: 100%;
+    }
+  .buttons {
+    z-index: 4;
+  }
+  .logo {
+    display: none;
+  }
+  .ul-nav {
+    display: none;
+  }
+  .burger-btn {
+    display: flex;
+  }
+  .container {
+    justify-content: flex-end;
+    height: 75px;
+  }
+}
+@media (min-width: 1270px) {
+    nav {
+        width: 100%;
+    }
+  .buttons {
+    position: relative;
+    z-index: 4;
+  }
+  .logo {
+    display: flex;
+  }
+  .ul-nav {
+    display: flex;
+  }
+  .burger-btn {
+    display: none;
+  }
+  .container {
+    justify-content: space-evenly;
+    height: auto;
+  }
+}
+</style>
